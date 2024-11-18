064b6853f8b4b2c8353c04cdce7ed0a6d97f4141
Tri Ta <DRT2175@NB33934.elcaNet.local>
2024-11-19 00:51:25 +0700

    BE: Create Product API

---
+++ aspnet-core/src/DrTFlower.Application/Products/ProductsAppService.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrTFlower.Products
+{
+    public class ProductsAppService : CrudAppService<Product, ProductDto>
+    {
+        public ProductsAppService(IRepository<Product, int> repository) : base(repository)
+        { 
+        }
+    }
+}
---
+++ aspnet-core/src/DrTFlower.Core/Products/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+
+namespace DrTFlower.Products
+{
+    public class ProductDto : EntityDto<int>
+    {
+        [AutoMapFrom(typeof(Product))]
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+    }
+}
