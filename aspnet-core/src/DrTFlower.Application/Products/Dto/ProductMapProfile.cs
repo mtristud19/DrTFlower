cc0fbc51d55ca5a3d968c6eab5ff09f2c6df22ea
Tri Ta <DRT2175@NB33934.elcaNet.local>
2024-12-02 18:33:58 +0700

    BE: Add ProductMapProfile

---
+++ aspnet-core/src/DrTFlower.Application/Products/Dto/ProductMapProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace DrTFlower.Products.Dto
+{
+    public class ProductMapProfile : Profile
+    {
+        public ProductMapProfile()
+        {
+            CreateMap<CreateProductsDto, Products>();
+
+            CreateMap<Products, ProductsDto>();
+
+            CreateMap<ProductsDto, Products>();
+        }
+    }
+}
