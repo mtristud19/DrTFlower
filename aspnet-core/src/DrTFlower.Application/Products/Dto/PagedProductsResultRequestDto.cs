5ea1e161b1a6252c5af36a7dd062d2c43a5f2265
Tri Ta <DRT2175@NB33934.elcaNet.local>
2024-12-02 18:31:29 +0700

    BE: Add Product App Service and page result

---
+++ aspnet-core/src/DrTFlower.Application/Products/Dto/PagedProductsResultRequestDto.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Application.Services.Dto;
+
+namespace DrTFlower.Products.Dto
+{
+    public class PagedProductsResultRequestDto : PagedResultRequestDto
+    {
+        public string Keyword { get; set; }
+    }
+}
---
+++ aspnet-core/src/DrTFlower.Application/Products/IProductsAppService.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Application.Services;
+using DrTFlower.Products.Dto;
+
+namespace DrTFlower.Products
+{
+    public interface IProductsAppService : IAsyncCrudAppService<ProductsDto, int, PagedProductsResultRequestDto, CreateProductsDto, ProductsDto>
+    {
+    }
+}
