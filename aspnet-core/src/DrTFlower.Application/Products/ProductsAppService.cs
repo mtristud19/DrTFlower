ed13d0e384d8ca976546b8dc69d52b3d0902c136
Tri Ta <DRT2175@NB33934.elcaNet.local>
2024-12-02 18:31:57 +0700

    BE: Add Product App Service

--- aspnet-core/src/DrTFlower.Application/Products/ProductsAppService.cs
+++ aspnet-core/src/DrTFlower.Application/Products/ProductsAppService.cs
@@ -1,21 +1,34 @@
 ﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
 using Abp.Authorization;
+using Abp.Domain.Entities;
 using Abp.Domain.Repositories;
 using DrTFlower.Authorization;
+using DrTFlower.Products.Dto;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DrTFlower.Products
 {
-
     [AbpAuthorize(PermissionNames.Pages_Products)]
-    public class ProductsAppService : CrudAppService<Product, ProductDto>
+    public class ProductsAppService : AsyncCrudAppService<Products, ProductsDto, int, PagedProductsResultRequestDto, CreateProductsDto, ProductsDto>, IProductsAppService
     {
-        public ProductsAppService(IRepository<Product, int> repository) : base(repository)
-        { 
+        public ProductsAppService(IRepository<Products, int> repository) : base(repository)
+        {
+        }
+
+        public override async Task<ProductsDto> CreateAsync(CreateProductsDto input)
+        {
+            return await base.CreateAsync(input);
+        }
+
+        public override async Task<ProductsDto> UpdateAsync(ProductsDto input)
+        {
+            return await base.UpdateAsync(input);
+        }
+        public override Task DeleteAsync(EntityDto<int> input)
+        {
+            return base.DeleteAsync(input);
         }
     }
 }
